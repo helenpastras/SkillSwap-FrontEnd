46027e56065cfbe4da80412b566178495061e6bc
helenpastras <helenapastras@hotmail.com>
2025-09-30 13:51:27 -0400

    updated Nav with child elements

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,33 +1,47 @@
-import { useContext } from 'react';
+
+import { useState } from 'react';
 import { Link } from 'react-router';
 
-import { UserContext } from '../../contexts/UserContext';
+const NavBar = ({ user, handleSignOut }) => {
+  const [showProfileMenu, setShowProfileMenu] = useState(false);
 
-const NavBar = () => {
-  const { user, setUser } = useContext(UserContext);
-
-  const handleSignOut = () => {
-    localStorage.removeItem('token');
-    setUser(null);
+  const handleToggleProfileMenu = () => {
+    setShowProfileMenu(prev => !prev);
   };
+
 
   return (
     <nav>
-      {user ? (
-        <ul>
-          <li>Welcome, {user.username}</li>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to="/skills">Browse Users</Link></li>
-          <li><Link to='/' onClick={handleSignOut}>Sign Out</Link></li>
-        </ul>
-      ) : (
-        <ul>
-          <li><Link to='/'>Home</Link></li>
-          <li><Link to='/sign-in'>Sign In</Link></li>
-          <li><Link to='/sign-up'>Sign Up</Link></li>
-        </ul>
-      )}
-    </nav>
+  {user ? (
+    <ul className="main-nav">
+      <li>Welcome, {user.username}</li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/skills">Browse Users</Link></li>
+
+      <li className="has-submenu">
+        <button type="button" onClick={handleToggleProfileMenu}>
+          Profile ▾
+        </button>
+        {showProfileMenu && (
+          <ul className="submenu">
+            <li><Link to="/profile">Profile</Link></li>
+            <li><Link to="/profile/swap-requests">My SwapRequests</Link></li>
+          </ul>
+        )}
+      </li>
+
+      <li>
+        <button onClick={handleSignOut}>Sign Out</button>
+      </li>
+    </ul>
+  ) : (
+    <ul className="main-nav">
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/sign-in">Sign In</Link></li>
+      <li><Link to="/sign-up">Sign Up</Link></li>
+    </ul>
+  )}
+</nav>
   );
 };
 
